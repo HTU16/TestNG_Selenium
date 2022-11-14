caf6c27c335bb8854f0924b38757f6cae1e29f09
mr.TALHA <h.talha.uysal@gmail.com>
2022-11-14 18:26:37 +0300

    selenium HTU 11.11.22

--- src/test/java/com/krafttechnologie/tests/day10_WebElements4/IframeTest.java
+++ src/test/java/com/krafttechnologie/tests/day10_WebElements4/IframeTest.java
@@ -55,4 +55,37 @@ public class IframeTest {
         textArea.clear();
         textArea.sendKeys("BİZ KİMSENİN VARLIĞIYLA VAR OLMADIK YOKLUĞUYLA DA YOK OLMAYIZ  3");
     }
+
+    @Test
+    public void nestedIfFrame(){
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+
+        // switch to middle frame and get MIDDLE text
+
+        driver.switchTo().frame("frame-top");
+        driver.switchTo().frame("frame-middle");
+
+        System.out.println("driver.findElement(By.cssSelector(\"#content\")).getText() = " + driver.findElement(By.cssSelector("#content")).getText());
+
+
+        // got to Right frame and get text
+
+        // driver.switchTo().parentFrame(); // selenium switch to parent
+
+        driver.switchTo().defaultContent(); // selenium switch the top
+        driver.switchTo().frame("frame-top"); // if we use defaultContent(), we should start first step
+
+        driver.switchTo().frame(2);
+        System.out.println("driver.findElement(By.tagName(\"Body\")).getText() = " + driver.findElement(By.tagName("Body")).getText());
+
+        // go to bottom frame and get text
+
+        driver.switchTo().defaultContent();
+        // driver.switchTo().parentFrame().switchTo().parentFrame(); // another way to go grandparent
+        driver.switchTo().frame("frame-bottom");
+        System.out.println("driver.findElement(By.tagName(\"Body\")).getText() = " + driver.findElement(By.tagName("Body")).getText());
+
+
+
+    }
 }
